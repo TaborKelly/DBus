fd5c1e3210e7e226daa21e714c2023db638abf67
Tabor Kelly <tabor.kelly@racepointenergy.com>
2019-03-27 15:27:30 -0400

    Better jazzy script: don't re-run swift package generate-xcodeproj if we don't need to.

--- jazzy.sh
+++ jazzy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-swift package generate-xcodeproj
-mkdir docs
+
+if [ ! -d "./DBus.xcodeproj" ]; then
+    swift package generate-xcodeproj
+fi
+
+if [ ! -d "./docs" ]; then
+    echo "Creating docs directory for jazzy output."
+    mkdir docs
+fi
+
 jazzy --clean \
       --output docs/ \
       -x -scheme,DBus-Package \
